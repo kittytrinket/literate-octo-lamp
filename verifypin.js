40dade8fe8d58e2e1e48a2f13559e71408c3221f
kittytrinket <105733549+kittytrinket@users.noreply.github.com>
2022-05-17 11:32:19 -0400

    Add files via upload

---
+++ verifypin.js
@@ -0,0 +1,24 @@
+var pins = ["3FAa3Aap2", "9JAg8Dmc5", "4DAj8Dfn1", "0DGm5Dbi9", "3BId8Bui0", "5BDe6Gzw9", "5IFf1Ikx0", "9AJj4Drp8", "2IAp8Fvz2", "4JIj7Hxe6", "9AIq4Iuy5", "6FBd3Iuy5", "5BJw9Ewr3", "2JEr9Ayv0", "1FDz4Jxp1", "4EJu0Cqd3", "3BEn6Aur6", "0BHp3Cea2", "8BGc3Hip3", "0FJm8Cxq1", "3EFj9Hdu1", "9AFr7Bze5", "1IDz6Aht2", "4HEk0Jhw4", "5FDp6Cri9", "7HAm5Cno8", "2CFi6Afv2", "3BJd5Dli6", "5FAh0Fbr8", "1EDn7Fjv5", "8CFi7Dgg0", "1IAm2Dye6", "1IGj5Goj7", "4JEi6Elg5", "8IEn3Fdn7", "1BDt2Gyy6", "4JAr1Ayz0", "8FIh4Fbh7", "7GGh3Abr9", "8HEm4Ipd3", "0FAj6Jqr2", "4AHj2Eiz5", "7JFd0Jvz8", "2ACr5Jdt4", "4FIw2Gov3", "6EEt8Izz3", "9DCm4Bvp9", "3BFf4Eig4", "4FCh1Bpv5", "5GEh1Fsi0", "4JDh3Gim9", "5HCv1Aoo7", "1FDf3Bwd3", "3BBv9Aqw2", "6HEj9Gqn1", "0BJd5Fsv5", "9JJm6Dtf1", "4EAn8Ihd3", "8DIf3Aij0", "1FBy3Fzj2",  "0DCv6Jtc4", "4IJd5Cof6", "3GHx7Jnt3", "2EDf6Fic8", "4JEy4Fdb3", "3EIh6Fqj7", "8HJe6Gcy7", "5AEg5Dun9", "9BFv4Cao8", "8DHt6Dcj7", "0JIi4Alw0", "3JGm1Cdd0", "5HCb9Bat6", "8DEt4Gvr7", "8IBk1Dhn2", "6DIe3Iup2", "6FFf2Fut6", "9HCv8Als8", "4JHy1Dsd7", "7GIm7Hgl1", "9HCb2Fus4", "8CHu6Hop0", "6CIz5Bxv3", "0EDt2Fbq1", "0JDy8Gve7", "0JGx5Dii2", "0BBi1Gkv1", "8BCl3Cvv9", "8JIm1Gsu3", "0BAl3Hof9", "5FBv3Dam1", "2FJc6Dzw1", "4DJz3Don3", "9BIv8Dkd8", "8FJx8Iiw8", "7JBs4Eec7", "1CDe4Dlt5", "1HJh7Ehz9", "3EEj4Gpq3", "7IHo9Bfx5", "9FDj2Bfx8", "6HCd6Bfd2", "1BBx5Bmx2", "8EAp2Eja2", "3GFw5Bln5", "6HFg2Czx8", "1IIk9Gux5", "7JIv3Diu4", "5CGz3Hni1", "3IGl3Asn1", "9IAg9Jti2", "0GEb7Gqj2", "5BCj3Dwm5", "7GFf2Akf7", "5BFm1Frc2", "9BHh6Fam5", "1AFq9Iki8", "0HBl2Fbn2", "4ABz6Hxv0", "6HCq1Hey5", "8JEi4Jle3", "9BIc7Dvk2", "2BCq0Dez3", "0ABp3Day6", "7AAj8Bdh8", "2JFv3Iyc8", "8FDk7Ixy9", "8FCj6Bdi2", "4EGb0Ghf5", "0FEm2Idg6", "2GJr4Gjd4", "6FIg5Jzr5", "1GIg8Hrk7", "8JDh0Gwk8", "2DAo7Geb3", "8GHi5Emo4", "2GGe4Iqr6", "1IEw6Eju4", "7HJk8Hep1", "1FCy5Gpg4", "5BIk8Fty5", "0CGn0Bpy3", "6DBk5Gvd2", "7BJz9Jbv6", "3AGw6Eta0", "4BHx4Jvc5", "3IBy0Hbb1", "7BIh0Cqk8", "0EDn0Bxl5", "6HIh7Eya2", "0EJq8Hsn2", "5IBe0Eof3", "2HEb4Jzj5", "2FAz3Iku3", "9HJg6Ddo1", "0HFa8Hfw7", "1JDu5Ila9", "6JGz5Byp8", "9BDg6Dzo1", "9GFq4Eyd6", "0GIu5Dyn5", "4IHv6Jyq8", "1FGo8Esx4", "0CIx9Cug4", "1CCj5Amw5", "2ACz8Eku9", "8DGd6Idc0", "3JAq4Hsd9", "7BCs1Dnf2", "1JIz2Ecs5", "0DIj5Alt7",  "3CFc4Bwv5", "6JAp7Jcr9", "0AFe9Jor1", "2CJn1Cmf6", "6DEp1Jui3", "8ECg3Eir1", "6JAy9Anl4", "5CIk6Fhl5", "6EId6Auf7", "7FBn5Jgk2", "2IHa4Ciu9", "8HDz5Hrc4", "4JEu3Dkh1", "7EHa2Exa3", "5DDb1Bnz8", "9AEh3Bfh0", "7AGk3Iyd4", "2IJm0Brs4", "2IJd9Dtc5", "2GAy9Fic9", "5EIm9Csl6", "3GCm8Bhl0", "6EGe6Jjn7", "4EHi3Cid0", "6DBn1Ipn4", "2ICw5Bkj7", "6DBp8Goa8", "7JGt0Fkq1", "9DIr4Ejj2"];
+
+function search() {
+    var txt = document.getElementById("inputfield").value;
+    var index = pins.indexOf(txt);
+
+    if (index !== -1){   
+        removed = pins.splice(txt);
+        console.log(removed)
+        let bingoNumbers = ["B1","B2","B3","B4","B5","B6","B7","B8","B9","B10","B11","B12","B13","B14","B15","I16","I17","I18","I19","I20","I21","I22","I23","I24","I25","I26","I27","I28","I29","I30","N31","N32","N33","N34","N35","N36","N37","N38","N39","N40","N41","N42","N43","N44","N45","G46","G47","G48","G49","G50","G51","G52","G53","G54","G55","G56","G57","G58","G59","G60","O61","O62","O63","O64","O65","O66","O67","O68","O69","O70","O71","O72","O73","O74","O75"];
+        let newline = "\r\n"
+        alert("Your Bingo Number is..." + newline + bingoNumbers[Math.floor(Math.random() * bingoNumbers.length)]);
+
+    
+    }else {
+        window.alert("Invalid.  Please try again!");
+    }     
+     
+        
+         
+    };
+                 
+    
+  
